10279b8238db369c3400194d6a62289bc0dfb216
Your Name <you@example.com>
2022-04-05 21:15:30 +0700

    Dung __name__

--- python.py
+++ python.py
@@ -2,12 +2,18 @@
 import urllib.request
 import re
 
-# Lấy source web
-html = urllib.request.urlopen(
-    "http://www.pythonchallenge.com/pc/def/equality.html").read().decode()
 
-# Lọc lấy đoạn thông tin cần tìn nằm trong <!-- --!>
-data = re.findall("<!--(.*)-->", html, re.DOTALL)[-1]
+def main():
+    # Lấy source web
+    html = urllib.request.urlopen(
+        "http://www.pythonchallenge.com/pc/def/equality.html").read().decode()
 
-# Tìm và in ra đoạn thông tin theo đúng yêu cầu được 3 kí tự hoa bao quanh
-print("".join(re.findall("[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+", data)))
+    # Lọc lấy đoạn thông tin cần tìn nằm trong <!-- --!>
+    data = re.findall("<!--(.*)-->", html, re.DOTALL)[-1]
+
+    # Tìm và in ra đoạn thông tin theo đúng yêu cầu được 3 kí tự hoa bao quanh
+    print("".join(re.findall("[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+", data)))
+
+
+if __name__ == '__main__':
+    main()
