76bb6e1b3fb8249699137f08de6a2ba457f028cb
Your Name <you@example.com>
2022-04-05 21:10:24 +0700

    Xong task py

---
+++ python.py
@@ -0,0 +1,13 @@
+# Import thư viện
+import urllib.request
+import re
+
+# Lấy source web
+html = urllib.request.urlopen(
+    "http://www.pythonchallenge.com/pc/def/equality.html").read().decode()
+
+# Lọc lấy đoạn thông tin cần tìn nằm trong <!-- --!>
+data = re.findall("<!--(.*)-->", html, re.DOTALL)[-1]
+
+# Tìm và in ra đoạn thông tin theo đúng yêu cầu được 3 kí tự hoa bao quanh
+print("".join(re.findall("[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+", data)))
